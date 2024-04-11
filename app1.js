931895738cf106d066889dc94285196c94f05888
nirmals01 <2nirmal@gmail.com>
2024-04-11 13:23:30 +0530

    initial commit

---
+++ app1.js
@@ -0,0 +1,27 @@
+const firstName = 'John'
+const age = 25
+
+const user = {
+    firstName, // object property shorthand
+    age1: age,
+    location: 'Chennai'
+};
+
+console.log(user);
+
+
+//object destructuring
+
+const prod = {
+    id: 100,
+    price: 300,
+    color: "red"
+}
+
+// const price = prod.price;
+// console.log(price);
+
+const {id: newid, color, discount = 5} = prod;
+console.log(color);
+console.log(newid);
+console.log(discount);
